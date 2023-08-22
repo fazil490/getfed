cc687e63947a8aa099749454996f4ed88f5e4d6b
Fazil <mdif4900@gmail.com>
2023-08-22 16:26:07 +0530

    Fetching external api using useEffect

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,9 +1,27 @@
 import RestaurantsList from "./RestaurantsList";
 import restList from "../utils/mockData";
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import Shimmer from "./Shimmer"
 
 const Main = () => {
-  const [listOfRest, setListOfRest] = useState(restList);
+  const [listOfRest, setListOfRest] = useState([]);
+
+  useEffect(() => {
+    fetchData();
+  }, [])
+
+  const fetchData = async () => {
+    const data = await fetch("https://www.swiggy.com/dapi/restaurants/list/v5?lat=13.0826802&lng=80.2707184&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING");
+    const json = await data.json();
+    console.log(json)
+    setListOfRest(json?.data?.cards[2]?.card?.card?.gridElements?.infoWithStyle?.restaurants)
+  }
+
+  if (listOfRest && listOfRest.length === 0) {
+    return (
+      <Shimmer />
+    )
+  }
 
   return (
     <main className="main-container">
@@ -13,7 +31,7 @@ const Main = () => {
           className="filter-btn"
           onClick={() => {
             const filteredList = listOfRest.filter(
-              (res) => res.info.avgRating > 4.2
+              (res) => res.info.avgRating > 4
             );
             setListOfRest(filteredList);
           }}
@@ -21,7 +39,7 @@ const Main = () => {
           Top Rated
         </button>
         <div className="rest-list">
-          {listOfRest.map((restaurant) => (
+          {listOfRest?.map((restaurant) => (
             <RestaurantsList key={restaurant.info.id} restData={restaurant} />
           ))}
         </div>
--- src/components/RestaurantsList.jsx
+++ src/components/RestaurantsList.jsx
@@ -1,26 +1,19 @@
-import {CDN_URL} from "../utils/constants"
+import { CDN_URL } from "../utils/constants";
 
 const RestaurantsList = ({ restData }) => {
-    const {cloudinaryImageId, name, cuisines, avgRating, costForTwo} = restData?.info;
-    return (
-      <div className="rest-cards">
-        <img
-          src={
-             CDN_URL+
-            cloudinaryImageId
-          }
-        />
-        <div className="rest-info">
-          <h3>{name}</h3>
-          <p className="cuisines">{cuisines.join(", ")}</p>
-          <div className="rest-rating-ttr-price">
-            <p id="rating">{avgRating}⭐</p>
-            <p>{restData.info.sla.deliveryTime} Mins to reach</p>
-            <p>{costForTwo}</p>
-          </div>
-        </div>
+  const { cloudinaryImageId, name, cuisines, avgRating, areaName} =
+    restData?.info;
+  return (
+    <div className="rest-cards">
+      <img src={CDN_URL + cloudinaryImageId} />
+      <div className="rest-info">
+        <h3>{name}</h3>
+        <p id="rating">{avgRating}⭐</p>
+        <p className="cuisines">{cuisines.join(", ")}</p>
+        <h5 className="location">{areaName}</h5>
       </div>
-    );
-  };
+    </div>
+  );
+};
 
 export default RestaurantsList;
