87c02661f43f89ddcbd89156ea4ddc70f1396bfd
Fazil <mdif4900@gmail.com>
2023-09-23 20:08:24 +0530

    custom hooks for listOfRest

--- src/utils/useListOfRest.jsx
+++ src/utils/useListOfRest.jsx
@@ -1,18 +1,18 @@
-// import { useState, useEffect } from "react"
+import { useState, useEffect } from "react"
 
-// const useListOfRest = () => {
-//     const [listOfRest, setListOfRest] = useState();
-//     useEffect(() => {
-//         fetchData()
-//     }, []);
+const useListOfRest = () => {
+    const [listOfRest, setListOfRest] = useState();
+    useEffect(() => {
+        fetchData()
+    }, []);
 
-//     const fetchData = async() => {
-//         const data = await fetch('https://www.swiggy.com/dapi/restaurants/list/v5?lat=13.0826802&lng=80.2707184&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING')
-//         const json = await data.json()
-//         setListOfRest(json?.data?.cards[2]?.card?.card?.gridElements?.infoWithStyle?.restaurants)
-//     }
+    const fetchData = async() => {
+        const data = await fetch('https://www.swiggy.com/dapi/restaurants/list/v5?lat=13.0826802&lng=80.2707184&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING')
+        const json = await data.json()
+        setListOfRest(json?.data?.cards[2]?.card?.card?.gridElements?.infoWithStyle?.restaurants)
+    }
 
-//     return listOfRest
-// }
+    return listOfRest
+}
 
-// export default useListOfRest;
+export default useListOfRest;
