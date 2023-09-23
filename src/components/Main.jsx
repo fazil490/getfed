fe42b5593fcd089bc2924b6df4dce850d8ad89ea
Fazil <mdif4900@gmail.com>
2023-09-23 20:14:51 +0530

    Higher Order Function

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,57 +1,61 @@
-import RestaurantsList from "./RestaurantsList";
-import restList from "../utils/mockData";
+import RestaurantsList, { availabilty } from "./RestaurantsList";
 import { useEffect, useState } from "react";
 import Shimmer from "./Shimmer";
 import { Link } from "react-router-dom";
 import useOnlineStatus from "../utils/useOnlineStatus";
+import useListOfRest from "../utils/useListOfRest";
 
 const Main = () => {
-
   const onlineStatus = useOnlineStatus();
-  const [listOfRest, setListOfRest] = useState([]);
 
-  useEffect(() => {
-    fetchData();
-  }, []);
+  const listOfRest = useListOfRest();
 
-  const fetchData = async () => {
-    const data = await fetch(
-      "https://www.swiggy.com/dapi/restaurants/list/v5?lat=13.0826802&lng=80.2707184&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING"
+  const IsOpen = availabilty(RestaurantsList);
+
+  if (onlineStatus === false)
+    return (
+      <div className="internet-error">
+        😕
+        <h3>OOPS!!!</h3>
+        <h4>Your Connection was Interrupted!</h4>
+        <h5>Try to connect to a wifi, network cables, modem or router</h5>
+      </div>
     );
-    const json = await data.json();
-    setListOfRest(
-      json?.data?.cards[2]?.card?.card?.gridElements?.infoWithStyle?.restaurants
-    );
-  };
-
-  if(onlineStatus === false) return (
-  <div className="internet-error">😕
-  <h3>OOPS!!!</h3>
-  <h4>Your Connection was Interrupted!</h4>
-  <h5>Try to connect to a wifi, network cables, modem or router</h5>
-  </div> 
-  )
 
   return listOfRest && listOfRest.length === 0 ? (
     <Shimmer />
   ) : (
-    <main className="main-container">
+    <main className="main-container w-[80%] my-4 mx-[auto] ">
+      <div className="w-[50%] mx-auto my-8">
+        <input
+          className="mx-4 px-4 py-2 rounded-md shadow-md w-[80%]"
+          type="search"
+          placeholder="Search for Restaurants and Cuisines"
+        ></input>
+        <button className="search-btn bg-red-400 hover:bg-red-500 shadow-md px-4 py-2 text-white rounded-md">
+          Search
+        </button>
+      </div>
       <div className="restuarants">
-        <h2>Restaurants offer online food delivery in Chennai</h2>
-        <button
-          className="filter-btn"
-          onClick={() => {
-            const filteredList = listOfRest.filter(
-              (res) => res.info.avgRating > 4
-            );
-            setListOfRest(filteredList);
-          }}
-        >
+        <h2 className="text-2xl font-medium py-2 my-2">
+          Restaurants offer online food delivery in Chennai
+        </h2>
+        <button className="filter-btn bg-slate-50 p-2 my-2 text-gray-600 border rounded-3xl hover:bg-slate-100">
           Top Rated
         </button>
-        <div className="rest-list">
+        <div className="rest-list m-4 grid gap-8 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
           {listOfRest?.map((restaurant) => (
-            <Link className="menu-routing" key={restaurant.info.id} to={"/restaurants/" + restaurant?.info?.id}> <RestaurantsList restData={restaurant} /></Link>
+            <Link
+              className="menu-routing"
+              key={restaurant.info.id}
+              to={"/restaurants/" + restaurant?.info?.id}
+            >
+              {restaurant?.info?.isOpen ? (
+                <IsOpen restData={restaurant} />
+              ) : (
+                <RestaurantsList restData={restaurant} />
+              )}
+            </Link>
           ))}
         </div>
       </div>
--- src/components/RestaurantsList.jsx
+++ src/components/RestaurantsList.jsx
@@ -1,20 +1,31 @@
 import { CDN_URL } from "../utils/constants";
 
 const RestaurantsList = (props) => {
-  const { restData } = props
+  const { restData } = props;
   const { cloudinaryImageId, name, cuisines, avgRating, areaName} =
     restData?.info;
   return (
-    <div className="rest-cards">
-      <img src={CDN_URL + cloudinaryImageId} />
-      <div className="rest-info">
-        <h4>{name}</h4>
-        <p id="rating">{avgRating}⭐</p>
-        <p className="cuisines">{cuisines.join(", ")}</p>
-        <h5 className="location">{areaName}</h5>
+    <div className="w-[250px] mx-[auto] my-4 shadow-md hover:shadow-xl">
+      <img className="w-[250px] h-[170px]" src={CDN_URL + cloudinaryImageId} />
+      <div className="text-left p-2">
+        <h4 className="text-xl font-medium my-1">{name}</h4>
+        <p id="rating" className="bg-green-600 w-fit p-1 my-1 text-white rounded-md">{avgRating}⭐</p>
+        <p className="text-gray-700 my-1">{cuisines.join(", ")}</p>
+        <h5 className="font-medium my-1">{areaName}</h5>
       </div>
     </div>
   );
 };
+
+export const availabilty = (RestaurantsList) => {
+  return (props) => {
+    return (
+      <div>
+        <label className="absolute mx-2 px-2 bg-amber-950 text-white">Promoted</label>
+        <RestaurantsList {...props}/>
+      </div>
+    )
+  }
+}
 
 export default RestaurantsList;
