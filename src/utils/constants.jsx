fc1033bb36904f6d7cb05438b86bf1122f722dbe
Fazil <mdif4900@gmail.com>
2024-01-22 16:03:39 +0530

    updated api extraction

--- src/utils/constants.jsx
+++ src/utils/constants.jsx
@@ -1,3 +1,3 @@
 export const CDN_URL = "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/"
 
-export const MENU_URL = "https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=13.0826802&lng=80.2707184&restaurantId="
+export const MENU_URL = "https://corsproxy.org/?https%3A%2F%2Fwww.swiggy.com%2Fdapi%2Fmenu%2Fpl%3Fpage-type%3DREGULAR_MENU%26complete-menu%3Dtrue%26lat%3D13.0826802%26lng%3D80.2707184%26restaurantId%3D"
